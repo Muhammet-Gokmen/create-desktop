5b055eacf3a32d89d235bc193d97186542ebc7a5
Muhammet-Gokmen <muhammetgokmen@gmail.com>
2022-06-06 14:49:31 +0200

    s3 added

---
+++ s3/abc.py
@@ -0,0 +1,7 @@
+x = “([{({})}])[([{{([])}}])](){()}”
+def isvalid(s):
+  while “()” in s or “[]” in s or “{}” in s :
+    s = s.replace(“[]“,”“).replace(“{}“,”“).replace(“()“,”“)
+  return s == “”
+isvalid(x)
+isvalid("(){}")
